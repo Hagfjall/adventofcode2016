dfdd116e3e763661f674e5c395beae8938837f94
Fredrik Hagfjäll <fredrik@hagfjall.se>
2016-12-24 11:51:24 +0100

    possible to generate checksum now

--- 04/day4.go
+++ 04/day4.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 	"fmt"
 	"strings"
-	"strconv"
-	"github.com/bradfitz/slice"
+	"sort"
+	"github.com/cznic/sortutil"
 )
 
 func main() {
@@ -22,45 +22,61 @@ func main() {
 		return
 	}
 	input := string(byte)
-	numberOfOccrencies := make(map[string]int)
+	//numberOfOccrencies := make(map[string]int)
 
 	for _, row := range strings.Split(input, "\n") {
-		numberOfOccrencies := calculateNumberOfOccurrences(row)
-		for index, nbr := range numberOfOccrencies {
-			print(string(index + 61) + " -> " + strconv.Itoa(nbr))
-		}
+		print(calculateChecksum(row))
+		//for index, nbr := range numberOfOccrencies {
+		//	//print(string(index + 61) + " -> " + strconv.Itoa(nbr))
+		//}
 	}
-	for key := range numberOfOccrencies {
-		fmt.Printf("%s\n", string(key))
-
-	}
+	//for key := range numberOfOccrencies {
+	//	fmt.Printf("%s\n", string(key))
+	//
+	//}
 }
 
-func calculateNumberOfOccurrences(string string) [28]int {
+//aaaaa-bbb-z-y-x-123[abxyz]
+func calculateChecksum(string string) string {
 	//alphabet := [...]string{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-	numberOfOccurrences := [28]int{0}
-	for index, character := range string {
-		if character == '['{
+	numberOfOccurrences := make(map[rune]int)
+	//count number of occurrences for all characters
+	for _, character := range string {
+		if character == '[' {
 			break;
 		}
-		if character < 'a'{
+		if character < 'a' {
 			continue
 		}
-		fmt.Printf("%c, %v, %v\n", string[index], character, character - 'a')
-		numberOfOccurrences[character - 'a']++
+		//fmt.Printf("%c, %v, %v\n", string[index], character, character - 'a')
+		numberOfOccurrences[character]++
 	}
 
-	return numberOfOccurrences
-}
+	//sort by occurrences and alphabetically
+	checksum := ""
+	n := map[int][]rune{}
+	var a []int
+	for k, v := range numberOfOccurrences {
+		n[v] = append(n[v], k)
+	}
+	for k := range n {
+		a = append(a, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
+	c := 0
+	Loop:
+	for _, k := range a {
+		sort.Sort(sortutil.RuneSlice(n[k]))
+		for _, s := range n[k] {
+			if c > 4 {
+				break Loop
+			}
 
-func getChecksum(numberOfOccurences []int) string{
-	alphabet := [...]string{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-
-	//get top five characters
-	top5Indexes := make(map[string]int)
-	for index, _ := range numberOfOccurences{
-
+			checksum += fmt.Sprintf("%c", s)
+			fmt.Printf("%d: %c, %d\n", c, s, k)
+			c++
+		}
 	}
 
-	return ""
+	return checksum
 }
