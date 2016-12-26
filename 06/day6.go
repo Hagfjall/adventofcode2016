e5a24a54e14e52b5f3d177bbf9a15a00331f7667
Fredrik Hagfjäll <fredrik@hagfjall.se>
2016-12-26 17:37:19 +0100

    implemented part one by counting each character per position

---
+++ 06/day6.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"log"
+	"io/ioutil"
+	"strings"
+)
+
+var input string
+var errorCorrectedMessage string
+
+const numberOfCharactersPerRow = 8
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	byteArray, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	input = string(byteArray)
+
+	var partTwoFlag bool
+	flag.BoolVar(&partTwoFlag, "partTwo", false, "run part two of the puzzle")
+	flag.Parse()
+
+	if partTwoFlag {
+		partTwo()
+	} else {
+		partOne()
+	}
+
+}
+
+func partOne() {
+	numberOfLines := strings.Count(input, "\n")
+	var mostCommonCharacter rune
+	var mostCommonCharacterCount int
+
+	for position := 0; position < numberOfCharactersPerRow; position++ {
+		mostCommonCharacterCount = 0
+		for character := 'a'; character <= 'z'; character++ {
+			characterCount := 0
+			for row := 0; row < numberOfLines; row++ {
+				if []rune(input)[position + (numberOfCharactersPerRow + 1 * row)] == character {
+					characterCount++
+				}
+			}
+			if characterCount > mostCommonCharacterCount {
+				mostCommonCharacterCount = characterCount
+				mostCommonCharacter = character
+			}
+		}
+		errorCorrectedMessage += string(mostCommonCharacter)
+	}
+	println(errorCorrectedMessage)
+}
+func partTwo() {
+	println("partTwo not implemented")
+}
